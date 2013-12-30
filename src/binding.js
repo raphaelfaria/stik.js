766095e70363e7086baed6ae4bf962691db9c548
Raphael Faria <raphael@codelogic.me>
2013-12-30 18:50:27 -0200

    Refactoring binding

--- src/binding.js
+++ src/binding.js
@@ -1,59 +1,26 @@
-window.stik || (window.stik = {});
-
 (function() {
 
   var bindingKey = "data-bind";
 
-  function Binding(template){
-    this.$$template = template;
-    this.$$bind = {};
-    this.$$elements = {};
-
-    this.$setup();
+  function Binding(binding, elements){
+    this.$$bind = binding;
+    this.$$elements = elements;
   }
 
-  Binding.prototype.$setAll = function() {
-    for (var prop in this.$$bind) {
-      if (this.$$bind.hasOwnProperty(prop)) {
-        this.$setOne(prop);
-      }
-    }
-  }
-
-  Binding.prototype.$setOne = function(prop) {
-    for(var i = 0; i < this.$$elements[prop].length; i++) {
-      if(this.$$elements[prop][i].nodeName === "INPUT" || this.$$elements[prop][i].nodeName === "TEXTAREA") {
-        this.$$elements[prop][i].value = this.$$bind[prop];
-      }
-      else {
-        this.$$elements[prop][i].textContent = this.$$bind[prop];
-      }
-    }
-  }
-
-  Binding.prototype.$set = function(val) {
-    if(!val) {
-      this.$setAll();
-    }
-    else {
-      this.$setOne(val);
-    }
-  };
-
-  Binding.prototype.$setup = function(){
-    var elements = this.$$template.querySelectorAll("[" + bindingKey + "]");
-
-    for(var i = 0; i < elements.length; i++) {
-      var attr = elements[i].getAttribute(bindingKey);
-      if(!this.$$bind[attr])
-        this.$$bind[attr] = "";
-    }
-    for (var prop in this.$$bind) {
-      if (this.$$bind.hasOwnProperty(prop)) {
-        this.$$elements[prop] = this.$$template.querySelectorAll("[" + bindingKey + "=" + prop + "]");
-      }
-    }
-  };
-
-  stik.Binding = Binding;
+  window.stik.Binding = Binding;
 })();
+
+
+
+// $viewBag.$set("item1", 12);
+
+// data-bind="item1" // item1 + elements
+// data-bind="item2" // item2 + elements
+
+// $set = function(key, value){
+//   this.bindings[key].update(value);
+// }
+
+// $update = function(value) {
+//   for elements.update(value)
+// }
--- src/view_bag.js
+++ src/view_bag.js
@@ -3,21 +3,8 @@
 
   function ViewBag(template){
     this.$$template = template;
+    this.$bindingSetup();
   }
-
-  ViewBag.prototype.$render = function(dataSet){
-    var fields, dataToBind;
-
-    fields = this.$fieldsToBind();
-
-    for (var i = 0; i < fields.length; i++) {
-      dataToBind = fields[i].getAttribute(bindingKey);
-
-      if (dataSet[dataToBind]) {
-        fields[i].textContent = dataSet[dataToBind];
-      }
-    }
-  };
 
   ViewBag.prototype.$fieldsToBind = function(){
     if (this.$$template.getAttribute(bindingKey)) {
@@ -27,6 +14,60 @@
     return this.$$template.querySelectorAll(
       "[" + bindingKey + "]"
     );
+  };
+
+  ViewBag.prototype.$bindingSetup = function(){
+    var allElements = this.$fieldsToBind(),
+        bind = {},
+        sortedElements = {};
+
+
+    for(var i = 0; i < allElements.length; i++) {
+      var attr = allElements[i].getAttribute(bindingKey);
+      if(!bind[attr])
+        bind[attr] = "";
+    }
+
+    this.$binding = new window.stik.Binding(bind, sortedElements);
+
+    for (var prop in bind) {
+      if (bind.hasOwnProperty(prop)) {
+        this.$binding.$$elements[prop] = this.$$template.querySelectorAll("[" + bindingKey + "=" + prop + "]");
+      }
+    }
+
+  };
+
+  ViewBag.prototype.$set = function(val) {
+    if(!val) {
+      this.$setAllBindings();
+    }
+    else {
+      this.$setOneBinding(val);
+    }
+  };
+
+  ViewBag.prototype.$setAllBindings = function() {
+    for (var prop in this.$binding.$$bind) {
+      if (this.$binding.$$bind.hasOwnProperty(prop)) {
+        this.$setOneBinding(prop);
+      }
+    }
+  }
+
+  ViewBag.prototype.$setOneBinding = function(prop) {
+    for(var i = 0; i < this.$binding.$$elements[prop].length; i++) {
+      if(this.$binding.$$elements[prop][i].nodeName === "INPUT" || this.$binding.$$elements[prop][i].nodeName === "TEXTAREA") {
+        this.$binding.$$elements[prop][i].value = this.$binding.$$bind[prop];
+      }
+      else {
+        this.$binding.$$elements[prop][i].textContent = this.$binding.$$bind[prop];
+      }
+    }
+  }
+
+  ViewBag.prototype.$updateBind = function(prop, value) {
+    this.$binding.$$bind[prop] = value;
   };
 
   window.stik.ViewBag = ViewBag;
