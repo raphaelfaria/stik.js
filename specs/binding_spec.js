11deaff008701c982682a9d8a0b4e3765a058bc9
Raphael Faria <raphael@codelogic.me>
2014-01-09 13:38:07 -0200

    Adding watchers and still not working test

--- specs/binding_spec.js
+++ specs/binding_spec.js
@@ -1,40 +1,65 @@
 describe("Binding", function(){
-  describe("#initialize", function(){
-    it("when ok", function(){
-      var binding, property, nodeElements, value;
+  // describe("#initialize", function(){
+  //   it("when ok", function(){
+  //     var binding, property, nodeElements, value;
 
-      property     = "name";
-      value        = "Anakin";
-      nodeElements = [
-        new DOMParser().parseFromString(
-          '<span data-bind="name"></span>', "text/xml"
-        ).firstChild
-      ]
+  //     property     = "name";
+  //     value        = "Anakin";
+  //     nodeElements = [
+  //       new DOMParser().parseFromString(
+  //         '<span data-bind="name"></span>', "text/xml"
+  //       ).firstChild
+  //     ];
 
-      binding = new stik.Binding(property, value, nodeElements);
+  //     binding = new stik.Binding(property, value, nodeElements);
 
-      expect(binding.$$property).toEqual(property);
-      expect(binding.$$value).toEqual(value);
-      expect(binding.$$nodeElements).toEqual(nodeElements);
-      expect(binding.$$nodeElements[0].textContent).toEqual("Anakin");
-    });
-  });
+  //     expect(binding.$$property).toEqual(property);
+  //     expect(binding.$$value).toEqual(value);
+  //     expect(binding.$$nodeElements).toEqual(nodeElements);
+  //     expect(binding.$$nodeElements[0].textContent).toEqual("Anakin");
+  //   });
+  // });
 
-  describe("#$updateValue", function(){
-    it("with one Node", function(){
+  // describe("#$updateValue", function(){
+  //   it("with one Node", function(){
+  //     var binding, nodeElements;
+
+  //     nodeElements = [
+  //       new DOMParser().parseFromString(
+  //         '<span data-bind="name"></span>', "text/xml"
+  //       ).firstChild
+  //     ];
+
+  //     binding = new stik.Binding("name", "", nodeElements);
+  //     binding.$updateValue("Darth");
+
+  //     expect(binding.$$value).toEqual("Darth");
+  //     expect(nodeElements[0].textContent).toEqual("Darth");
+  //   });
+  // });
+
+  describe("#watchers", function(){
+    it("watch input", function(){
       var binding, nodeElements;
 
       nodeElements = [
         new DOMParser().parseFromString(
+          '<input type="text" data-bind="name">', "text/xml"
+        ).firstChild,
+        new DOMParser().parseFromString(
           '<span data-bind="name"></span>', "text/xml"
         ).firstChild
-      ]
+      ];
 
       binding = new stik.Binding("name", "", nodeElements);
-      binding.$updateValue("Darth");
+      nodeElements[0].value = "Darth";
+
+      console.info('\n');
+      console.info(nodeElements[0].value);
+      console.info('\n');
 
       expect(binding.$$value).toEqual("Darth");
-      expect(nodeElements[0].textContent).toEqual("Darth");
+      expect(nodeElements[1].textContent).toEqual("Darth");
     });
   });
 });
--- src/binding.js
+++ src/binding.js
@@ -27,5 +27,32 @@
     }
   };
 
+  Binding.prototype.$setupWatchers = function(){
+    var that = this;
+
+    for(var i = 0; i < this.$$nodeElements.length; i++) {
+      if (this.$$nodeElements[i].nodeName === "INPUT" || this.$$nodeElements[i].nodeName === "TEXTAREA" || this.$$nodeElements[i].nodeName === "SELECT") {
+        this.$$nodeElements[i].addEventListener("input", changeHandle, false);
+      }
+      else {
+        this.$$nodeElements[i].addEventListener("change", changeHandle, false);
+      }
+    }
+
+    function changeHandle(evt){
+      var target = evt.target,
+          value;
+
+      if (target.nodeName === "INPUT" || target.nodeName === "TEXTAREA" || target.nodeName === "SELECT") {
+        value = target.value;
+      }
+      else {
+        value = target.textContent;
+      }
+
+      that.$updateValue(value);
+    }
+  };
+
   window.stik.Binding = Binding;
 })();
